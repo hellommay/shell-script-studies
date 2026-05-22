a30441662578556d4b9379f907660673551dc39f
hellommay <hellommay@hotmail.com>
2026-05-22 19:15:26 -0300

    script projeto incial

---
+++ newfile-python/criar-python.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# nome do projeto
+read -p "Nome do projeto: " projeto
+
+# criar e entrar na pasta
+mkdir "$projeto"
+
+cd "$projeto" || exit
+
+# criando e ativando ambiente virtual
+python3 -m venv venv
+
+source venv/bin/activate
+
+# estrutura basica
+mkdir src tests
+
+touch README.md
+touch .gitignore
+touch src/main.py
+
+#instalando libs iniciais e gerando requeriments
+pip install requests
+
+pip freeze > requirements.txt
+
+#conteudo dentro do .gitignore
+cat > .gitignore <<EOF
+venv/
+__pycache__/
+*.pyc
+.env
+.DS_Store
+EOF
+
+
+echo "Projeto Python criado com sucesso. "
